9db7a32fec5de9ea21a0b7565335eaa5f2544f7b
OrnellaGrigolato <62859666+OrnellaGrigolato@users.noreply.github.com>
2024-06-13 12:06:37 -0300

    SQL Query

---
+++ queryToCreateTable.sql
@@ -0,0 +1,12 @@
+CREATE TABLE CRM_HORAS_TRABAJADAS (
+    id INT IDENTITY(1,1) PRIMARY KEY,   -- Definición del campo autoincremental y clave primaria
+    cliente NVARCHAR(255) NOT NULL,     -- Cliente al que se le realizó el trabajo
+    tipo NVARCHAR(255) NOT NULL,        -- (Abonado, Facturable,...)
+	horaDeInicio TIME,					-- Hora en que se inició el trabajo
+	cantidad DECIMAL(3,2),						-- Cantidad de horas trabajadas
+    comentario NVARCHAR(255) NOT NULL,  -- Comentarios sobre el trabajo realizado
+    ID_Usuario INT,						-- Id del trabajador
+	fechaDeInicio DATE,					-- Fecha en la que se realizó el trabajo
+    CONSTRAINT FK_HorasTrabajadas_Usuario FOREIGN KEY (ID_Usuario)
+        REFERENCES CRM_VENDEDORES(id_vendedor)
+);
